324407f0e17dd4894639f1a316073cabacd5d9c2
Phil Wamba <philwamba@gmail.com>
2019-08-16 07:34:36 +0300

    added material & quill editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactQuill from 'react-quill';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import styles from './styles';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: '' } // You can also pass a Quill Delta here
+    this.handleChange = this.handleChange.bind(this)
+  }
+ 
+  handleChange(value) {
+    this.setState({ text: value })
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+    <div className="editor">
+      <Grid container direction="row">
+        <Grid item md={2} lg={2}>
+          <Paper className={classes.paper}>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Add Note
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item md={10} lg={10}>
+          <ReactQuill 
+            value={this.state.text}
+            onChange={this.handleChange}
+            className={classes.editorContainer} />
+        </Grid>
+      </Grid>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+const styles = theme => ({
+    paper: {
+        height: `100vh`,
+        width: `100%`,
+        borderRadius: 0,
+    },
+    button: {
+        width: `100%`,
+        borderRadius: 0,
+    },
+    editorContainer: {
+        height: '100%',
+        boxSizing: 'border-box'
+    }
+});
+
+export default styles;
